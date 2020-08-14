3ba9371bab01d45eeb83046d31610a6e0b1a05ff
Maksim Gichyan <maxim.gichyan@gmail.com>
2020-08-14 23:21:14 +0300

    solution for 03/02

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,13 +4,39 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.lineBuffer = '';
+    this.lineSeparator = os.EOL;
   }
 
   _transform(chunk, encoding, callback) {
+    const linesArray = chunk.toString().split(this.lineSeparator);
+    // Если в чанке есть разделитель, то к первому элементу добавляем текущий буфер и пушем
+    if (linesArray.length > 1) {
+      this.push(this.lineBuffer + linesArray[0]);
+      this.lineBuffer = '';
+    }
+    // Пущем все промежуточнэе строки
+    for (const newChunk of linesArray.slice(1, linesArray.length - 1)) {
+      this.push(newChunk);
+    }
+    // Добавляем в буфер в последний элемент массива
+    this.lineBuffer += linesArray[linesArray.length - 1];
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.lineBuffer);
   }
 }
 
 module.exports = LineSplitStream;
+
+const lines = new LineSplitStream({ encoding: 'utf-8' });
+lines.on('data', (chunk) => {
+  console.log(chunk);
+});
+lines.write('a');
+lines.write(`b${os.EOL}c`);
+lines.write(`d${os.EOL}e`);
+lines.write('f');
+lines.end();
