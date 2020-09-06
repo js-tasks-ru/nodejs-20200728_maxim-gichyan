a1b770fff926e9be8744d22079f99eb6dd5d90f3
Maksim Gichyan <maxim.gichyan@gmail.com>
2020-09-06 21:46:03 +0300

    solution for 08-authentication

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,26 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+module.exports = async function authenticate(
+  strategy,
+  email,
+  displayName,
+  done
+) {
+  try {
+    if (!email) {
+      done(null, false, 'Не указан email');
+    } else {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        const newUser = await new User({
+          email: email,
+          displayName: displayName,
+        }).save();
+        return done(null, newUser);
+      } else {
+        return done(null, user);
+      }
+    }
+  } catch (err) {
+    done(err);
+  }
 };
