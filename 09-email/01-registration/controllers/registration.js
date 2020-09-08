ecae20a9c7c64471e7ec2f8b5c7d7c61a6054946
Maksim Gichyan <maxim.gichyan@gmail.com>
2020-09-08 13:05:09 +0300

    solution for 09-email

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -3,9 +3,35 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
-
+  const token = uuid();
+  const user = new User({
+    email: ctx.request.body.email,
+    displayName: ctx.request.body.displayName,
+    verificationToken: token,
+  });
+  await user.setPassword(ctx.request.body.password);
+  await user.save();
+  const mail = await sendMail({
+    template: 'confirmation',
+    locals: { token: token },
+    to: ctx.request.body.email,
+    subject: 'Подтвердите почту',
+  });
+  ctx.body = { status: 'ok' };
+  return next();
 };
 
 module.exports.confirm = async (ctx, next) => {
-  
+  const user = await User.findOne({
+    verificationToken: ctx.request.body.verificationToken,
+  });
+  if (!user)
+    ctx.throw(401, 'Ссылка подтверждения недействительна или устарела');
+  user.verificationToken = undefined;
+  ctx.request.body.email = user.email;
+  ctx.request.body.password = '*';
+  await user.save();
+  const token = await ctx.login(user);
+  ctx.body = { token };
+  return next();
 };
